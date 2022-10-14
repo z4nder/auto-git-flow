26111e18f15b555fc916ad5cf03a2d7de5cd3e4e
Gustavo Araujo <gusttavo212@gmail.com>
2022-10-14 16:46:39 -0300

    AAA

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,47 @@
 use octocrab::{Octocrab};
 use std::env;
-use octocrab::Error;
+use octocrab::Error as OctocrabError;
 use std::process::Command;
 use std::str;
+use dotenv::dotenv;
+use std::error::Error;
 
 #[derive(Debug)]
+enum Branch {
+    BUGFIX,
+    HOTFIX,
+    FEATURE
+}
+
+
 struct GitHubUser {
     id: String,
     name: String,
 }
 
+struct GitHubRepo {
+    url: String,
+    name: String
+}
+
+struct GitHubPullRequest {
+    name: String,
+    from: String,
+    to: String
+}
+
+
 // # TODO
 // Get current repo
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<(), OctocrabError> {
+    dotenv().ok();
+
     let octocrab = instance_octocrab();
-    let owner = get_current_user(&octocrab).await;
-    
+    let owner = get_current_user(&octocrab).await;    
     let repo = get_repo_name();
 
-    println!("Current User => {:?}", owner);
-    
     // Use Tokio
     // create_pr(&octocrab, owner, repo).await;
 
@@ -47,15 +67,34 @@ async fn get_current_user(octocrab: &Octocrab) -> GitHubUser{
     }
 }
 
-fn get_repo_name(){
+fn get_repo_name() -> GitHubRepo{
+    let repo_path = match env::var("REPO_PATH") {
+        Ok(value) => value,
+        Err(_) => String::from("./")
+
+    };
+
     let output = Command::new("git")
-    .arg("config")
-    .arg("--get")
-    .arg("remote.origin.url")
-    .output().unwrap();
-    let response =  String::from_utf8(output.stdout).unwrap();
-    println!("AA => {}", response);
+        .current_dir(repo_path)
+        .arg("config")
+        .arg("--get")
+        .arg("remote.origin.url")
+        .output().unwrap();
+
+    let url = String::from_utf8(output.stdout).unwrap();   
+   
+    return GitHubRepo {
+        name: between(&url, "/", ".git"),
+        url: url
+    };  
 }
+
+fn between(line: &String, start: &str, end: &str) -> String {
+    let start_bytes = line.find(start).unwrap_or(0); 
+    let end_bytes = line.find(end).unwrap_or(line.len()); 
+    return String::from(&line[start_bytes+1..end_bytes]);
+}
+
 async fn create_pr(octocrab: &Octocrab, owner: &str, repo: &str) {
     let pr =  octocrab 
     .pulls(owner, repo)
@@ -69,3 +108,16 @@ async fn create_pr(octocrab: &Octocrab, owner: &str, repo: &str) {
     }
 }
 
+async fn request_review() -> Result<(), Box<dyn Error>>{
+    // https://docs.github.com/en/rest/pulls/review-requests#request-reviewers-for-a-pull-request
+    // let gh = env::var("GIT_TOKEN").expect("$GIT_TOKEN is not set");
+    // let route = "https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER/requested_reviewers\";
+    // let resp = reqwest::get("https://httpbin.org/ip")
+    // .await?
+    // .text()
+    // .await?;
+
+    // println!("RESPONSE => {:#?}", resp);
+
+    // Ok(())
+}
